ea8acd07f52d80f7829459987b2ea54fb16dfb32
bshin27 <125755440+bshin27@users.noreply.github.com>
2025-06-24 19:44:09 -0500

    add HI.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def index
-    render json: { message: "Hello Class - From Leon and also hello from Amelia, Marylin, Tova and from Michal add whatever i want, hi its faigy" }
+    render json: { message: "Hello Class - From Leon and also hello from Amelia, Marylin, Tova and from Michal add whatever i want, hi its faigy Hi!" }
   end
 end
