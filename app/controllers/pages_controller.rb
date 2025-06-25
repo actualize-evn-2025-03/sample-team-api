17aa9fd395520900788fe1cb7194088c8be46966
bshin27 <125755440+bshin27@users.noreply.github.com>
2025-06-24 19:36:12 -0500

    add mu name to json

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def index
-    render json: { message: "Hello Class - From Leon and also hello from Amelia add whatever i want" }
+    render json: { message: "Hello Class - From Leon and also hello from Amelia and from Michal add whatever i want" }
   end
 end
