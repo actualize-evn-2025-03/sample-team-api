f3b624121bff65b5221346be567b2dfdd3720bac
tovawieds <wiedermanntova@gmail.com>
2025-06-24 20:40:00 -0400

    add name

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def index
-    render json: { message: "Hello Class - From Leon and also hello from Amelia" }
+    render json: { message: "Hello Class - From Leon and also hello from Amelia and from Tova" }
   end
 end
