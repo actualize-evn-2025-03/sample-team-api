65566225ee6090f4583e8023eb74f80cfd7ec715
maryhiguera <marylin0higuera@gmail.com>
2025-06-24 19:39:35 -0500

    add my name

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def index
-    render json: { message: "Hello Class - From Leon and also hello from Amelia add whatever i want" }
+    render json: { message: "Hello Class - From Leon, Marylin and also hello from Amelia add whatever i want" }
   end
 end
